4e642da7bea774270f1c3bb4d7f9050abb52a4f9
Charles Weiss <cweiss1995@gmail.com>
2023-10-18 23:06:24 +0800

    Infrastructure - Pipenv, modules (somewhat) (#1)
    * Setting up pipenv
    
    * Playing with modules
    
    * Setting up pylance with pipenv and adding numpy for arrays

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello World");
+import tictactoe
+
+def main():
+    tictactoe.OpponentMakesMove(5)
+    print(f"We are in the module {__name__}")
+
+if __name__ == "__main__":
+    SystemExit(main())
---
+++ tictactoe.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+def OpponentMakesMove(boardstate: [int]) -> list[int]:
+    print(f"Opponent makes {boardstate}")
+    np.array()
